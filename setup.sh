ed7fb322aae4e2f12259b83dc5434b02ea102098
whoops <31858202+whoopsme1337@users.noreply.github.com>
2025-07-29 01:56:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-echo "🚀 Installing rustup..."
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
-
-echo "🎯 install Dependensi..."
+echo "🎯 Installing system dependencies..."
 sudo apt update && sudo apt install -y build-essential pkg-config libssl-dev git curl
+
+# Check if Rust is already installed
+if command -v rustc &> /dev/null
+then
+    echo "✅ Rust is already installed. Skipping rustup installation."
+else
+    echo "🚀 Installing rustup..."
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+fi
 
 echo "📦 Cloning Pipe repo..."
 git clone https://github.com/PipeNetwork/pipe.git
